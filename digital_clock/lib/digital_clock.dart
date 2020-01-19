1515526f9e474ece5cfc648d3ecf8e78600344ae
Vukan-Markovic <vukan.markovic97@gmail.com>
2020-01-19 19:44:40 +0100

    final commit

---
+++ digital_clock/lib/digital_clock.dart
@@ -0,0 +1,122 @@
+import 'dart:async';
+import 'package:flutter_clock_helper/model.dart';
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+enum _Element {
+  background,
+  text,
+  shadow,
+}
+
+final _lightTheme = {
+  _Element.background: Colors.white,
+  _Element.text: Colors.amber,
+  _Element.shadow: Colors.black,
+};
+
+final _darkTheme = {
+  _Element.background: Colors.black,
+  _Element.text: Colors.white,
+  _Element.shadow: Colors.amber,
+};
+
+class DigitalClock extends StatefulWidget {
+  const DigitalClock(this.model);
+  final ClockModel model;
+
+  @override
+  _DigitalClockState createState() => _DigitalClockState();
+}
+
+class _DigitalClockState extends State<DigitalClock> {
+  DateTime _dateTime = DateTime.now();
+  Timer _timer;
+
+  @override
+  void initState() {
+    super.initState();
+    widget.model.addListener(_updateModel);
+    _updateTime();
+    _updateModel();
+  }
+
+  @override
+  void didUpdateWidget(DigitalClock oldWidget) {
+    super.didUpdateWidget(oldWidget);
+    if (widget.model != oldWidget.model) {
+      oldWidget.model.removeListener(_updateModel);
+      widget.model.addListener(_updateModel);
+    }
+  }
+
+  @override
+  void dispose() {
+    _timer?.cancel();
+    widget.model.removeListener(_updateModel);
+    widget.model.dispose();
+    super.dispose();
+  }
+
+  void _updateModel() {
+    setState(() {});
+  }
+
+  void _updateTime() {
+    setState(() {
+      _dateTime = DateTime.now();
+      _timer = Timer(
+        Duration(seconds: 1) - Duration(milliseconds: _dateTime.millisecond),
+        _updateTime,
+      );
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final colors = Theme.of(context).brightness == Brightness.light
+        ? _lightTheme
+        : _darkTheme;
+    final hour =
+        DateFormat(widget.model.is24HourFormat ? 'HH' : 'hh').format(_dateTime);
+    final minute = DateFormat('mm').format(_dateTime);
+    final second = DateFormat('ss').format(_dateTime);
+    final fontSize = MediaQuery.of(context).size.width / 3.5;
+    final offset = -fontSize / 7;
+    final defaultStyle = TextStyle(
+      color: colors[_Element.text],
+      fontSize: fontSize,
+      shadows: [
+        Shadow(
+          blurRadius: 0,
+          color: colors[_Element.shadow],
+          offset: Offset(10, 0),
+        ),
+      ],
+    );
+
+    return Container(
+      color: colors[_Element.background],
+      child: Center(
+        child: DefaultTextStyle(
+          style: defaultStyle,
+          child: Stack(
+            children: <Widget>[
+              Align(alignment: Alignment.topCenter, child: Text(hour)),
+              Align(
+                  alignment: Alignment.bottomLeft,
+                  child: Padding(
+                      padding: EdgeInsets.only(left: 8.0),
+                      child: Text(minute))),
+              Align(
+                  alignment: Alignment.bottomRight,
+                  child: Padding(
+                      padding: EdgeInsets.only(right: 12.0),
+                      child: Text(second))),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
